4ec771882857a4aa92ed7ce87a9294fc4949a78a
jleev <justinviegelahn@gmail.com>
2016-05-16 21:30:09 -0500

    added model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ActiveRecord::Base
+
+  TAX = 0.09
+
+def sale_message
+ if price.to_i >= 75
+      " Discount Item"
+ else
+      " On Sale"
+ end      
+
+end
+
+def tax
+  price.to_i * TAX
+end
+
+def total
+  price.to_i + tax
+end
+
 end
